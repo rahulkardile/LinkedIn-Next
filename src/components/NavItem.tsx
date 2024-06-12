81fa7dde576b693f9f68bb1f007d4d35770f3ba8
Rahul kardile <rahulkardile321@gmail.com>
2024-06-12 17:01:29 +0530

    navigation is updated

--- src/components/NavItem.tsx
+++ src/components/NavItem.tsx
@@ -1,49 +1,57 @@
-import { NavItemType } from '@/types/NavItemsType';
-import { Bell, BriefcaseBusiness, Home, MessageCircleMore, User } from 'lucide-react'
-import Link from 'next/link';
-import React from 'react';
+import { NavItemType } from "@/types/NavItemsType";
+import {
+  Bell,
+  BriefcaseBusiness,
+  Home,
+  MessageCircleMore,
+  User,
+} from "lucide-react";
+import Link from "next/link";
+import React from "react";
 
 const NavItems: NavItemType[] = [
-    {
-        src: "/home",
-        icon: <Home />,
-        text: "Home",
-    },
-    {
-        src: "/networks",
-        icon: <User />,
-        text: "My Network",
-    },
-    {
-        src: "/job",
-        icon: <BriefcaseBusiness />,
-        text: "Jobs",
-    },
-    {
-        src: "/message",
-        icon: <MessageCircleMore />,
-        text: "Home",
-    },
-    {
-        src: "/notification",
-        icon: <Bell />,
-        text: "Notification",
-    },
-]
+  {
+    src: "/",
+    icon: <Home />,
+    text: "Home",
+  },
+  {
+    src: "/networks",
+    icon: <User />,
+    text: "My Network",
+  },
+  {
+    src: "/job",
+    icon: <BriefcaseBusiness />,
+    text: "Jobs",
+  },
+  {
+    src: "/message",
+    icon: <MessageCircleMore />,
+    text: "Home",
+  },
+  {
+    src: "/notification",
+    icon: <Bell />,
+    text: "Notification",
+  },
+];
 
 const NavItem = () => {
   return (
-    <div className='flex gap-8'>
-        {
-            NavItems.map((item, index)=>(
-                <div key={index} className="flex flex-col items-center cursor-pointer text-gray-500 hover:text-black">
-                    <span>{item.icon}</span>
-                    <Link className='text-xs' href={item.src}>{item.text}</Link>
-                </div>
-            ) )
-        }
+    <div className="flex gap-8 select-none">
+      {NavItems.map((item, index) => (
+        <Link
+          key={index}
+          href={item.src}
+          className="flex flex-col items-center cursor-pointer text-gray-500 hover:text-black"
+        >
+          <span className="text-xs">{item.icon}</span>
+          <span className="text-xs">{item.text}</span>
+        </Link>
+      ))}
     </div>
-  )
-}
+  );
+};
 
-export default NavItem
+export default NavItem;
