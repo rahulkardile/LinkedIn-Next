5cf1b4c604eafa696bdd641c2484a2f009babaaa
Rahul kardile <rahulkardile321@gmail.com>
2024-06-16 06:06:58 +0530

    Feed SetUp

--- src/components/Feed.tsx
+++ src/components/Feed.tsx
@@ -1,8 +1,13 @@
 import React from 'react'
+import PostInput from './PostInput'
+import Posts from './Posts'
 
 const Feed = () => {
   return (
-    <div>Feed</div>
+    <div className="flex-1 p-3 border border-black">
+      <PostInput />
+      <Posts />
+    </div>
   )
 }
 
---
+++ src/components/PostInput.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const PostInput = () => {
+  return (
+    <div>PostInput</div>
+  )
+}
+
+export default PostInput
