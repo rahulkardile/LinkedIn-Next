96529bb0684b2d2678c1383901aecebdc374baf7
Rahul kardile <rahulkardile321@gmail.com>
2024-06-17 17:44:55 +0530

    user model has been create

--- models/user.model.ts
+++ models/user.model.ts
@@ -1,5 +1,46 @@
-import mongoose from "mongoose";
+import mongoose, { Document, Model } from "mongoose";
 
-const UserSchema = new mongoose.Schema({
-    
-})
+export interface IUser {
+  firstName: string;
+  lastName: string;
+  userId: string;
+  profileImage?: string;
+  bio?: string;
+}
+
+export interface IUserDocument extends IUser, Document {
+  createdAt: Date;
+  updatedAt: Date;
+}
+
+const UserSchema = new mongoose.Schema<IUserDocument>(
+  {
+    firstName: {
+      type: String,
+      required: true,
+    },
+    lastName: {
+      type: String,
+      required: true,
+    },
+    profileImage: {
+      type: String,
+      required: true,
+    },
+    bio: {
+      type: String,
+      default: "",
+      required: true,
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+const User: Model<IUserDocument> = mongoose.model<IUserDocument>(
+  "User",
+  UserSchema
+);
+
+export default User;
